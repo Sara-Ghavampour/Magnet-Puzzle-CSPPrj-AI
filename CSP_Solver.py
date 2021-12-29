d9591a5869c58f84494c646261499e667a0f4f34
sara1301 <ghavams2001@gmail.com>
2021-12-29 13:56:32 +0330

    forward check fixed

--- CSP_Solver.py
+++ CSP_Solver.py
@@ -92,6 +92,27 @@ def check_consistency_with_neibours(board,sqr,pairSqr):  ### check if there is a
     return True
 
 
+def get_neibours(board,sqr):  ### get all neibours for a sqr
+    n = len(board)
+    m = len(board[0])
+    x= sqr.x
+    y=sqr.y
+    neibours=[]
+
+    if sqr.magnet_value!=None :   # if we assigned to this variable(sqr) -> now we can return the neibours for upcoming uses in forward check
+        if x-1>=0 : # left neibour
+            neibours.append(board[x-1][y])
+        elif x+1<n : # right neibour
+            neibours.append(board[x+1][y])
+        elif y-1>=0 :# downside neibour 
+            neibours.append(board[x][y-1])
+        elif y+1<n : # upside neibour 
+            neibours.append(board[x][y+1])
+
+    return neibours
+
+
+
 def is_consistent(board,sqr,pairSqr):  ## check satisfaction for rows and cols and ++ or -- in neibours
     n = len(board)  ## rows
     m = len(board[0])  ## cols
@@ -157,6 +178,18 @@ def satisfaction_test(board):  ## check if constraint for each row and col is sa
 
     return True         
 
+def forward_check(board,sqr,pairSqr):
+    sqr_neighbors=get_neibours(board,sqr)
+    pairSqr_neighbors=get_neibours(board,pairSqr)
+    print("sqr_neighbors , pairSqr_neighbors: ",sqr_neighbors,pairSqr_neighbors)
+    for i in range(0,len(sqr_neighbors)): # FC for sqr 
+        if sqr.magnet_value in sqr_neighbors[i].domain_square:
+            sqr_neighbors[i].domain_square.remove(sqr.magnet_value)
+    for i in range(0,len(pairSqr_neighbors)): # FC for pairSqr 
+        if pairSqr.magnet_value in pairSqr_neighbors[i].domain_square:
+            pairSqr_neighbors[i].domain_square.remove(pairSqr.magnet_value)    
+
+
 
 
 def backTrack_Csp(board):
@@ -209,68 +242,8 @@ def backTrack_Csp(board):
            
             curr_board[pairSqr.x][pairSqr.y].domain_square.remove(curr_board[pairSqr.x][pairSqr.y].magnet_value )
         #if is_consistent(curr_board,curr_board[pairSqr.x][pairSqr.y],curr_board[pairSqr.x][pairSqr.y]):
+        forward_check(curr_board,curr_board[sqr.x][sqr.y],curr_board[pairSqr.x][pairSqr.y])
         if(backTrack_Csp(curr_board)):
             return True
     return False
 
-
-
-# def backTrack_Csp(board):
-#     # print('csppp')
-#     # print_board(board)
-#     if assignment_is_done(board)==True:
-#         print("assignment_is_done: ")
-#         print_board(board)
-#         return True
-
-#     sqr = mrv_Heuristic(board)
-#     pairSqr = get_the_pair(sqr,board)
-#     print("Sqr : ",sqr.x,sqr.y,sqr.value)
-#     print("pairSqr : ",pairSqr.x,pairSqr.y,pairSqr.value)
-#     print(" domain fo sqr: ",sqr.domain_square)
-#     print(" domain of pairsqr: ",pairSqr.domain_square)
-
-#     ### iterate the domain : 
-#     for i in range (len(sqr.domain_square)):
-#     #for i in range (len(sqr.domain_square)):
-#         sqr_value = board[sqr.x][sqr.y].domain_square[i]
-#         curr_board = copy.deepcopy(board)
-
-#         # sqr_copy = copy.deepcopy(sqr)
-#         # pairSqr_copy =copy.deepcopy(pairSqr) 
-#         if sqr_value=='+':
-#             pair_value='-'
-#             # curr_board[sqr.x][sqr.y].domain_square.remove(sqr_value)
-#             # curr_board[pairSqr.x][pairSqr.y].domain_square.remove(pair_value)
-#         elif sqr_value=='-':
-#             pair_value='+'
-#         elif sqr_value=='0':
-#             pair_value='0'
-
-#         curr_board[sqr.x][sqr.y].magnet_value = sqr_value  ## sqr_copy
-#         curr_board[pairSqr.x][pairSqr.y].magnet_value = pair_value  ##pairSqr_copy
-       
-#         print_board(curr_board)
-#         print("curr sgr value : ",curr_board[sqr.x][sqr.y].magnet_value )
-#         print("curr pairsgr value : ",curr_board[pairSqr.x][pairSqr.y].magnet_value)
-
-#         ## remove these values from domain
-#         print("new domain fo sqr before remove : ",curr_board[sqr.x][sqr.y].domain_square)
-          
-#         print("new domain of pairsqr before remove before if: ",curr_board[pairSqr.x][pairSqr.y].domain_square,pairSqr.x)  
-#         if curr_board[sqr.x][sqr.y].magnet_value in curr_board[sqr.x][sqr.y].domain_square:
-#             print("*")
-#             curr_board[sqr.x][sqr.y].domain_square.remove(sqr_value)
-#             print("new domain fo sqr afterrr remove: ",curr_board[sqr.x][sqr.y].domain_square,sqr.x)
-#         print("new domain of pairsqr before remove : ",curr_board[pairSqr.x][pairSqr.y].domain_square,pairSqr.x)  
-#         print(" some arbittt: ",curr_board[3][3].domain_square , 3)  
-#         if curr_board[pairSqr.x][pairSqr.y].magnet_value in curr_board[pairSqr.x][pairSqr.y].domain_square:
-#             print("**")
-#             curr_board[pairSqr.x][pairSqr.y].domain_square.remove(pair_value)
-#             print("new domain of pairsqr  afterrr remove: ",curr_board[pairSqr.x][pairSqr.y].domain_square)
-
-#         print("new domain fo sqr: ",curr_board[sqr.x][sqr.y].domain_square)
-#         print("new domain of pairsqr : ",curr_board[pairSqr.x][pairSqr.y].domain_square)
-
-#         print("Sqr : ",sqr.x,sqr.y,curr_board[sqr.x][sqr.y].magnet_value,curr_board[sqr.x][sqr.y].value)
-#         print("pairSqr : ",pairSqr.x,pairSqr.y,curr_board[pairSqr.x][pairSqr.y].magnet_value,curr_board[pairSqr.x][pairSqr.y].value )
